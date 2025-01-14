414a649e813aea98921deb86760c6afd6f58ba3f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-14 12:17:43 +0100

    Implementation utilizing Talon rpc

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,26 @@
+import { NodeIo, TalonRpcServer } from "talon-rpc";
 import * as vscode from "vscode";
-
-import { NativeIo } from "./nativeIo";
 import CommandRunner from "./commandRunner";
+import { RPC_DIR_NAME } from "./constants";
 import { FocusedElementType } from "./types";
 
 export async function activate(context: vscode.ExtensionContext) {
-  const io = new NativeIo();
+  const commandRunner = new CommandRunner();
+  const io = new NodeIo(RPC_DIR_NAME);
+  const rpc = new TalonRpcServer(io, commandRunner.runCommand);
+
   await io.initialize();
 
-  const commandRunner = new CommandRunner(io);
   let focusedElementType: FocusedElementType | undefined;
+
+  io.dirPath;
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "command-server.runCommand",
       async (focusedElementType_: FocusedElementType) => {
         focusedElementType = focusedElementType_;
-        await commandRunner.runCommand();
+        await rpc.executeRequest();
         focusedElementType = undefined;
       }
     ),
--- src/uninstall.ts
+++ src/uninstall.ts
@@ -1,8 +1,10 @@
-import { getCommunicationDirPath } from "./paths";
 import { sync as rimrafSync } from "rimraf";
+import { NodeIo } from "talon-rpc";
+import { RPC_DIR_NAME } from "./constants";
 
 function main() {
-  rimrafSync(getCommunicationDirPath(), { disableGlob: true });
+  const io = new NodeIo(RPC_DIR_NAME);
+  rimrafSync(io.dirPath, { disableGlob: true });
 }
 
 main();
